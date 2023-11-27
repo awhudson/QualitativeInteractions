3e61fc740314c46a830a9ac74a2a7f059bc0e36a
awhudson <33168986+awhudson@users.noreply.github.com>
2023-11-26 21:43:58 -0800

    Update QualIntSims.R

--- QualIntSims.R
+++ QualIntSims.R
@@ -1,17 +1,19 @@
 set.seed(206)
 
-source('QualInt.R')
+my.wd <- "/Users/awhudson/Dropbox/MethodsResearch/QualitativeInteractions/Code/"
+setwd(my.wd)
+source('Method/RelativeDifferenceTest-rev-su22.R')
 
 ##########################################
 ### Simulation Settings
 ##########################################
 
 theta.1 <- 1
-theta.2 <- seq(-1, 1, .1)
-n <- c(50, 100)  #200, 400)
-kappa <- c(2, 4)
+theta.2 <- seq(-1, 1, .05)
+n <- c(50, 100, 200, 400, 800, 1600)
+kappa <- c(1.5, 2, 4, 8)
 sigma <- 1
-no.reps <- 5000
+no.reps <- 1000
 alpha <- .05
 
 ###########################################
@@ -22,6 +24,7 @@ alpha <- .05
 p.vals.RD <- array(NA, dim = c(length(theta.2), length(n), no.reps, length(kappa)))
 p.vals.OM <- array(NA, dim = c(length(theta.2), length(n), no.reps, length(kappa)))
 p.vals.NAIVE <- array(NA, dim = c(length(theta.2), length(n), no.reps))
+p.vals.QUANT <- array(NA, dim = c(length(theta.2), length(n), no.reps))
 kmax <- array(NA, dim = c(length(theta.2), length(n), no.reps))
 
 for(i in 1:length(theta.2)) {
@@ -41,8 +44,14 @@ for(i in 1:length(theta.2)) {
                      coefficients(mod.2)[2])
       standard.error <- c(vcov(mod.1)[2,2] ** .5,
                           vcov(mod.2)[2,2] ** .5)
+      
+      # Perform test for quantitative interactions
+      quant.test.stat <- (theta.hat[1] - theta.hat[2])^2/(sum(standard.error^2))
+      p.vals.QUANT[i,j,k] <- pchisq(quant.test.stat, df = 1, lower.tail = FALSE)
+      
       # Peform naive test
       p.vals.NAIVE[i,j,k] <- NaiveTest(theta.hat, standard.error, .05)
+      
       # Perform likelihood ratio test
       for(l in 1:length(kappa)) {
         p.vals.RD[i,j,k,l] <- RelDifLRT(theta.hat, standard.error,
@@ -52,14 +61,19 @@ for(i in 1:length(theta.2)) {
       }
       # Obtain kappa_max statistic
       kmax[i,j,k] <- kappa.max(theta.hat, standard.error,
-                               c(n[j], n[j]), alpha)
+                               c(n[j], n[j]), alpha, tol = .01)
     }
   }
 }
 
+output = list(p.vals.RD = p.vals.RD, p.vals.OM = p.vals.OM, p.vals.NAIVE = p.vals.NAIVE, kmax = kmax,
+              n = n, kappa = kappa, theta.2 = theta.2)
+
 #######################################
 ### Save Output
 #######################################
-output = list(p.vals.RD = p.vals.RD, p.vals.OM = p.vals.OM, p.vals.NAIVE = p.vals.NAIVE, kmax = kmax,
-              n = n, kappa = kappa, theta.2 = theta.2)
-save(output, "sim_output.rda")
+output = list(p.vals.RD = p.vals.RD, p.vals.OM = p.vals.OM, 
+              p.vals.NAIVE = p.vals.NAIVE, p.vals.QUANT = p.vals.QUANT,
+              kmax = kmax, n = n, kappa = kappa, theta.2 = theta.2)
+save(output,
+     file = "/Users/awhudson/Dropbox/MethodsResearch/QualitativeInteractions/Code/Sims-rev-su2022/sim_output.rda")
